17a8eabd5a112c25765613aafae1bd7b05cf021e
Iusan Adelin-Andrei <adelin.iusan@stud.ubbcluj.ro>
2021-11-07 23:09:15 +0200

    work in progress

---
+++ UI/console2.py
@@ -0,0 +1,55 @@
+from Domain.Obiect import creeazaObiect, toString
+from Logic.CRUD import adaugaObiect, stergeObiect, modificaObiect
+from Logic.Functionalitati import changeLocation
+from UI.console import uiAdaugaObiect
+
+
+def MenuHelp():
+    print("Spre exemplu pentru comenzi din"
+          "CRUD:add,id, nume, descriere, pret, locatie")
+    print("Pentru afisarea tuturor obiectelor :show_all")
+    print("Pentru mutarea dintr-o locatie in alta a obiectelor:mutare,locatie_initiala,locatie_noua")
+    print("Iar pentru folosirea mai multor actiuni la o singura citire vom proceda astfel:add,id, nume, "
+          "descriere, pret, locatie;show_all;mutare,locatieInitiala,locatieNoua")
+
+def add(id, nume, descriere, pret, locatie, lista):
+
+    try:
+        lista = adaugaObiect(id, nume, descriere, pret, locatie, lista)
+    except ValueError as ve:
+        print("Eroare: ", ve)
+    print("Obiectul a fost adaugata")
+    return lista
+
+def delete(id, lista):
+    try:
+        lista = stergeObiect(id, lista)
+    except ValueError as ve:
+        print("Eroare: ", ve)
+    print("Stergerea a fost facuta")
+    return lista
+
+def showall(lista):
+    for obiect in lista:
+        print(toString(obiect))
+
+
+def runNewMenu(lista):
+    while True:
+        MenuHelp()
+        optiune = input("Dati comenzile: ")
+        optiuni = optiune.split(';')
+        for comenzi in optiuni:
+            sir = comenzi.split(',')
+            if sir[0] == "add":
+                id = sir[1]
+                nume = sir[2]
+                descriere = sir[3]
+                pret = sir[4]
+                locatie = sir[5]
+                lista = add(id, nume, descriere, pret, locatie, lista)
+            if sir[0] == "delete":
+                id = sir[1]
+                lista = delete(id, lista)
+            if sir[0] == "showall":
+                showall(lista)
--- main.py
+++ main.py
@@ -1,9 +1,30 @@
+from Domain.Obiect import creeazaObiect
+from Logic.CRUD import adaugaObiect
 from Test.TestAll import runAllTests
 from UI.console import runMenu
+from UI.console2 import runNewMenu
 
+
+def meniuri():
+    print("1. Meniul vechi")
+    print("2. Meniul nou")
+    print("x. Iesire")
 
 def main():
     runAllTests()
-    runMenu([])
+
+    lista = []
+    lista = adaugaObiect(1, "pc", "descriere 1", 4000, "loc1", lista)
+    lista = adaugaObiect(2, "laptop", "descriere 2", 1000, "loc1", lista)
+    lista = adaugaObiect(3, "birou", "descriere 3", 3000, "loc2", lista)
+    lista = adaugaObiect(4, "masa", "descriere 4", 2000, "loc2", lista)
+
+    while True:
+        meniuri()
+        optiune = input("Dati optiunea: ")
+        if optiune == "1":
+            runMenu(lista)
+        elif optiune == "2":
+            runNewMenu(lista)
 
 main()
